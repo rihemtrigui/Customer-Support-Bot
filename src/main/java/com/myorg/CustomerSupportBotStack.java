70d0c02587b88be1f4681695f3996db72474148d
Rihem TRIGUI <rtrigui@sofrecom.fr>
2025-03-13 09:17:47 +0100

    First commit

--- src/main/java/com/myorg/CustomerSupportBotStack.java
+++ src/main/java/com/myorg/CustomerSupportBotStack.java
@@ -1,24 +1,240 @@
 package com.myorg;
 
+import software.amazon.awscdk.*;
+import software.amazon.awscdk.services.iam.*;
+import software.amazon.awscdk.services.lambda.*;
+import software.amazon.awscdk.services.dynamodb.*;
+import software.amazon.awscdk.services.lambda.Runtime;
 import software.constructs.Construct;
-import software.amazon.awscdk.Stack;
-import software.amazon.awscdk.StackProps;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.lex.CfnBot;
+import software.amazon.awscdk.services.lex.CfnBot.PromptSpecificationProperty;
+import software.amazon.awscdk.services.lex.CfnBotAlias;
+import software.amazon.awscdk.services.lex.CfnBot.BotAliasLocaleSettingsProperty;
+
+
+import java.util.List;
+import java.util.Map;
 
 public class CustomerSupportBotStack extends Stack {
-    public CustomerSupportBotStack(final Construct scope, final String id) {
-        this(scope, id, null);
-    }
-
     public CustomerSupportBotStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
+        ITable Clients_Database = Table.fromTableName(this, "Clients_Database", "Clients_Database");
 
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "CustomerSupportBotQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
+        Role lambdaRole = Role.Builder.create(this, "LambdaExecutionRole")
+                .assumedBy(new ServicePrincipal("lambda.amazonaws.com"))
+                .managedPolicies(List.of(
+                        ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole"),
+                        ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaRole")
+                ))
+                .build();
+
+        Clients_Database.grantFullAccess(lambdaRole);
+
+        Function SimpleHandler = Function.Builder.create(this, "SimpleHandler")
+                .runtime(Runtime.JAVA_17)
+                .handler("com.myorg.SimpleHandler::handleRequest")
+                .code(Code.fromAsset("target"))
+                .memorySize(512)
+                .timeout(Duration.seconds(30))
+                .role(lambdaRole)
+                .environment(Map.of(
+                        "DYNAMODB_TABLE_NAME", Clients_Database.getTableName()
+                ))
+                .build();
+        CfnBot.SlotTypeProperty actionSlotType = CfnBot.SlotTypeProperty.builder()
+                .name("ActionType")
+                .description("Defines actions the user can take on an order")
+                .slotTypeValues(List.of(
+                        CfnBot.SlotTypeValueProperty.builder()
+                                .sampleValue(CfnBot.SampleValueProperty.builder().value("update shipping address").build())
+                                .build(),
+                        CfnBot.SlotTypeValueProperty.builder()
+                                .sampleValue(CfnBot.SampleValueProperty.builder().value("cancel order").build())
+                                .build()
+                ))
+                .valueSelectionSetting(CfnBot.SlotValueSelectionSettingProperty.builder()
+                        .resolutionStrategy("ORIGINAL_VALUE")
+                        .build())
+                .build();
+
+        CfnBot.BotLocaleProperty botLocale = CfnBot.BotLocaleProperty.builder()
+                .localeId("en_US")
+                .nluConfidenceThreshold(0.8)
+                .slotTypes(List.of(actionSlotType))
+                .intents(List.of(
+                        createIntent("change_order", "Handles order-related requests", actionSlotType),
+                        createFallbackIntent()
+                ))
+                .build();
+
+        CfnBot lexBot = CfnBot.Builder.create(this, "CustomerSupportBot")
+                .name("CustomerSupportBot")
+                .dataPrivacy(Map.of("ChildDirected", false))
+                .idleSessionTtlInSeconds(300)
+                .roleArn(lambdaRole.getRoleArn())
+                .botLocales(List.of(botLocale))
+                .build();
+
+        CfnBotAlias botAlias = CfnBotAlias.Builder.create(this, "CustomerSupportBotAlias")
+                .botId(lexBot.getAttrId())
+                .botAliasName("BotAlias")
+                .botVersion("1")
+                .botAliasLocaleSettings(List.of(
+                        CfnBotAlias.BotAliasLocaleSettingsItemProperty.builder()
+                                .localeId("en_US")
+                                .botAliasLocaleSetting(CfnBotAlias.BotAliasLocaleSettingsProperty.builder()
+                                        .enabled(true)
+                                        .codeHookSpecification(CfnBotAlias.CodeHookSpecificationProperty.builder()
+                                                .lambdaCodeHook(CfnBotAlias.LambdaCodeHookProperty.builder()
+                                                        .lambdaArn(SimpleHandler.getFunctionArn())
+                                                        .codeHookInterfaceVersion("1.0")
+                                                        .build())
+                                                .build())
+                                        .build())
+                                .build()
+                ))
+                .build();
+        botAlias.addDependency(lexBot);
+        SimpleHandler.addPermission("LexInvokePermission",
+                Permission.builder()
+                        .principal(new ServicePrincipal("lex.amazonaws.com"))
+                        .action("lambda:InvokeFunction")
+                        .sourceArn(lexBot.getAttrArn())
+                        .build()
+        );
+    }
+
+    private CfnBot.IntentProperty createIntent(String name, String description, CfnBot.SlotTypeProperty actionSlotType) {
+        CfnBot.PromptSpecificationProperty orderNumberPrompt = CfnBot.PromptSpecificationProperty.builder()
+                .messageGroupsList(List.of(
+                        CfnBot.MessageGroupProperty.builder()
+                                .message(
+                                        CfnBot.MessageProperty.builder()
+                                                .plainTextMessage(
+                                                        CfnBot.PlainTextMessageProperty.builder()
+                                                                .value("Please provide your order number.")
+                                                                .build()
+                                                )
+                                                .build()
+                                )
+                                .build()
+                ))
+                .maxRetries(2)
+                .allowInterrupt(true)
+                .build();
+
+        CfnBot.PromptSpecificationProperty shippingAddressPrompt = CfnBot.PromptSpecificationProperty.builder()
+                .messageGroupsList(List.of(
+                        CfnBot.MessageGroupProperty.builder()
+                                .message(
+                                        CfnBot.MessageProperty.builder()
+                                                .plainTextMessage(
+                                                        CfnBot.PlainTextMessageProperty.builder()
+                                                                .value("Please provide the new shipping address.")
+                                                                .build()
+                                                )
+                                                .build()
+                                )
+                                .build()
+                ))
+                .maxRetries(2)
+                .allowInterrupt(true)
+                .build();
+        CfnBot.PromptSpecificationProperty actionTypePrompt = CfnBot.PromptSpecificationProperty.builder()
+                .messageGroupsList(List.of(
+                        CfnBot.MessageGroupProperty.builder()
+                                .message(
+                                        CfnBot.MessageProperty.builder()
+                                                .plainTextMessage(
+                                                        CfnBot.PlainTextMessageProperty.builder()
+                                                                .value("How would you like to modify your order ?")
+                                                                .build()
+                                                )
+                                                .build()
+                                )
+                                .build()
+                ))
+                .maxRetries(2)
+                .allowInterrupt(true)
+                .build();
+
+
+        CfnBot.SlotProperty orderNumberSlot = CfnBot.SlotProperty.builder()
+                .name("OrderNumber")
+                .description("Order number")
+                .slotTypeName("AMAZON.Number")
+                .valueElicitationSetting(CfnBot.SlotValueElicitationSettingProperty.builder()
+                        .slotConstraint("Required")
+                        .promptSpecification(orderNumberPrompt)
+                        .build())
+                .build();
+
+        CfnBot.SlotProperty actionTypeSlot = CfnBot.SlotProperty.builder()
+                .name("ActionType")
+                .description("Action to perform")
+                .slotTypeName(actionSlotType.getName())
+                .valueElicitationSetting(CfnBot.SlotValueElicitationSettingProperty.builder()
+                        .slotConstraint("Required")
+                        .promptSpecification(actionTypePrompt)
+                        .build())
+                .build();
+
+        CfnBot.SlotProperty shippingAddressSlot = CfnBot.SlotProperty.builder()
+                .name("ShippingAddress")
+                .description("New shipping address")
+                .slotTypeName("AMAZON.City")
+                .valueElicitationSetting(CfnBot.SlotValueElicitationSettingProperty.builder()
+                        .slotConstraint("Required")
+                        .promptSpecification(shippingAddressPrompt)
+                        .build())
+                .build();
+
+        List<CfnBot.SlotPriorityProperty> slotPriorities = List.of(
+                CfnBot.SlotPriorityProperty.builder()
+                        .priority(1)
+                        .slotName("OrderNumber")
+                        .build(),
+                CfnBot.SlotPriorityProperty.builder()
+                        .priority(2)
+                        .slotName("ActionType")
+                        .build(),
+                CfnBot.SlotPriorityProperty.builder()
+                        .priority(3)
+                        .slotName("ShippingAddress")
+                        .build()
+        );
+
+        return CfnBot.IntentProperty.builder()
+                .name(name)
+                .description(description)
+                .fulfillmentCodeHook(CfnBot.FulfillmentCodeHookSettingProperty.builder()
+                        .enabled(true)
+                        .build())
+                .slots(List.of(orderNumberSlot, actionTypeSlot, shippingAddressSlot))
+                .slotPriorities(slotPriorities)
+                .sampleUtterances(List.of(
+                        CfnBot.SampleUtteranceProperty.builder().utterance("I want to {ActionType} ").build(),
+                        CfnBot.SampleUtteranceProperty.builder().utterance("Please {ActionType} on order {OrderNumber} ").build(),
+                        CfnBot.SampleUtteranceProperty.builder().utterance("Can you {ActionType} to {ShippingAddress} ?").build(),
+                        CfnBot.SampleUtteranceProperty.builder().utterance("I want to change my shipping address to {ShippingAddress} ?").build(),
+                        CfnBot.SampleUtteranceProperty.builder().utterance("Can you change my shipping address to {ShippingAddress} ?").build(),
+                        CfnBot.SampleUtteranceProperty.builder().utterance("I want to change order {OrderNumber}").build(),
+                        CfnBot.SampleUtteranceProperty.builder().utterance("I want to {ActionType}").build(),
+                        CfnBot.SampleUtteranceProperty.builder().utterance("I need to {ActionType}").build(),
+                        CfnBot.SampleUtteranceProperty.builder().utterance("{ActionType}").build(),
+                        CfnBot.SampleUtteranceProperty.builder().utterance("I want to change order").build(),
+                        CfnBot.SampleUtteranceProperty.builder().utterance("{ShippingAddress}").build(),
+                        CfnBot.SampleUtteranceProperty.builder().utterance("I need to update the {ShippingAddress}").build()
+                ))
+                .build();
+    }
+
+    private CfnBot.IntentProperty createFallbackIntent() {
+        return CfnBot.IntentProperty.builder()
+                .name("FallbackIntent")
+                .description("Handles unknown user inputs")
+                .parentIntentSignature("AMAZON.FallbackIntent")
+                .build();
     }
 }
---
+++ src/main/java/com/myorg/SimpleHandler.java
@@ -0,0 +1,180 @@
+package com.myorg;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SimpleHandler implements RequestHandler<Map<String, Object>, Map<String, Object>> {
+
+    private final DynamoDbClient dynamoDbClient = DynamoDbClient.create();
+    private static final String TABLE_NAME = "Clients_Database";
+
+    @Override
+    public Map<String, Object> handleRequest(Map<String, Object> event, Context context) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            JsonNode rootNode = objectMapper.valueToTree(event);
+            JsonNode sessionState = rootNode.path("sessionState");
+            JsonNode intent = sessionState.path("intent");
+            String intentName = intent.has("name") ? intent.path("name").asText() : "UnknownIntent";
+            JsonNode slotsNode = intent.path("slots");
+            Map<String, Object> slots = objectMapper.convertValue(slotsNode, new TypeReference<Map<String, Object>>() {});
+            JsonNode sessionAttributes = rootNode.path("sessionAttributes");
+            Map<String, Object> sessionAttributesMap = new HashMap<>();
+            if (sessionAttributes != null && !sessionAttributes.isMissingNode()) {
+                sessionAttributesMap = objectMapper.convertValue(sessionAttributes, new TypeReference<Map<String, Object>>() {});
+            }
+            context.getLogger().log("Session Attributes: " + sessionAttributesMap);
+
+            for (String slotName : slots.keySet()) {
+                String slotValue = getOrRestoreSlot(slotName, slots, sessionAttributesMap, context);
+                if (slotValue != null && !slotValue.isEmpty()) {
+                    sessionAttributesMap.put(slotName, slotValue);
+                }
+            }
+
+            String orderNumberText = getOrRestoreSlot("OrderNumber", slots, sessionAttributesMap, context);
+            if (orderNumberText == null || orderNumberText.isEmpty()) {
+                return buildLexResponse(intentName, "Please provide your order number.", "InProgress", sessionAttributesMap, "OrderNumber");
+            }
+
+            int orderNumber;
+            try {
+                orderNumber = Integer.parseInt(orderNumberText);
+                if (orderNumber <= 0) {
+                    return buildLexResponse(intentName, "Order number must be a positive integer. Please provide a valid order number.", "InProgress", sessionAttributesMap, "OrderNumber");
+                }
+            } catch (NumberFormatException e) {
+                return buildLexResponse(intentName, "Invalid order number format. Please provide a valid order number.", "InProgress", sessionAttributesMap, "OrderNumber");
+            }
+
+            if (!orderExists(orderNumber)) {
+                return buildLexResponse(intentName, "Order number " + orderNumber + " does not exist. Please check and try again.", "Failed", sessionAttributesMap, null);
+            }
+
+            String action = getOrRestoreSlot("Action", slots, sessionAttributesMap, context);
+            if (action == null || action.isEmpty()) {
+                return buildLexResponse(intentName, "How can I help you with your order ?", "InProgress", sessionAttributesMap, "Action");
+            }
+
+            if (action.equalsIgnoreCase("update shipping address")) {
+                String shippingAddress = getOrRestoreSlot("ShippingAddress", slots, sessionAttributesMap, context);
+                if (shippingAddress == null || shippingAddress.isEmpty()) {
+                    return buildLexResponse(intentName, "Please provide the new shipping address.", "InProgress", sessionAttributesMap, "ShippingAddress");
+                }
+                updateShippingAddress(orderNumber, shippingAddress);
+                String confirmationMessage = "The shipping address for order " + orderNumber + " has been successfully updated to " + shippingAddress + ".";
+                return buildLexResponse(intentName, confirmationMessage, "Fulfilled", sessionAttributesMap, null);
+            }
+
+            if (action.equalsIgnoreCase("cancel order")) {
+                deleteOrder(orderNumber);
+                String confirmationMessage = "Order number " + orderNumber + " has been successfully deleted.";
+                return buildLexResponse(intentName, confirmationMessage, "Fulfilled", sessionAttributesMap, null);
+            }
+
+            return buildLexResponse(intentName, "How can I help you with your order ?", "InProgress", sessionAttributesMap, "Action");
+
+        } catch (Exception e) {
+            context.getLogger().log("Error processing Lex event: " + e.getMessage());
+            return buildLexResponse("UnknownIntent", "Error processing request. Please try again later.", "Failed", Map.of(), null);
+        }
+    }
+
+    private String getOrRestoreSlot(String slotName, Map<String, Object> slots, Map<String, Object> sessionAttributesMap, Context context) {
+        String slotValue = null;
+        if (slots.get(slotName) != null) {
+            JsonNode slotNode = new ObjectMapper().valueToTree(slots.get(slotName));
+            if (slotNode.has("value") && slotNode.path("value").has("interpretedValue")) {
+                slotValue = slotNode.path("value").path("interpretedValue").asText();
+                sessionAttributesMap.put(slotName, slotValue);
+                context.getLogger().log(slotName + " stored in session attributes: " + slotValue);
+            }
+        } else if (sessionAttributesMap.containsKey(slotName)) {
+            slotValue = (String) sessionAttributesMap.get(slotName);
+            context.getLogger().log("Restored " + slotName + " from session attributes: " + slotValue);
+        }
+        return slotValue;
+    }
+
+    private Map<String, Object> buildLexResponse(String intentName, String message, String intentState, Map<String, Object> sessionAttributes, String slotToElicit) {
+        Map<String, Object> dialogAction = new HashMap<>();
+
+        if (slotToElicit != null && (
+                slotToElicit.equals("OrderNumber") ||
+                        slotToElicit.equals("Action") ||
+                        slotToElicit.equals("ShippingAddress"))) {
+
+            dialogAction.put("type", "ElicitSlot");
+            dialogAction.put("slotToElicit", slotToElicit);
+        } else {
+            dialogAction.put("type", "Close");
+        }
+
+        Map<String, Object> slots = new HashMap<>();
+        for (String key : sessionAttributes.keySet()) {
+            slots.put(key, Map.of("value", Map.of("interpretedValue", sessionAttributes.get(key))));
+        }
+
+        return Map.of(
+                "sessionState", Map.of(
+                        "dialogAction", dialogAction,
+                        "intent", Map.of(
+                                "name", intentName,
+                                "state", intentState,
+                                "slots", slots
+                        ),
+                        "sessionAttributes", sessionAttributes
+                ),
+                "messages", new Object[]{
+                        Map.of("contentType", "PlainText", "content", message)
+                }
+        );
+    }
+
+    private boolean orderExists(int orderNumber) {
+        GetItemRequest request = GetItemRequest.builder()
+                .tableName(TABLE_NAME)
+                .key(Map.of("order_number", AttributeValue.builder().n(String.valueOf(orderNumber)).build()))
+                .build();
+        GetItemResponse response = dynamoDbClient.getItem(request);
+        return response.hasItem();
+    }
+
+    private void deleteOrder(int orderNumber) {
+        Map<String, AttributeValue> key = new HashMap<>();
+        key.put("order_number", AttributeValue.builder().n(String.valueOf(orderNumber)).build());
+
+        DeleteItemRequest deleteRequest = DeleteItemRequest.builder()
+                .tableName(TABLE_NAME)
+                .key(key)
+                .build();
+
+        dynamoDbClient.deleteItem(deleteRequest);
+    }
+
+
+    private void updateShippingAddress(int orderNumber, String newAddress) {
+        Map<String, AttributeValue> key = new HashMap<>();
+        key.put("order_number", AttributeValue.builder().n(String.valueOf(orderNumber)).build());
+
+        Map<String, AttributeValue> attributeValues = new HashMap<>();
+        attributeValues.put(":newAddress", AttributeValue.builder().s(newAddress).build());
+
+        UpdateItemRequest request = UpdateItemRequest.builder()
+                .tableName(TABLE_NAME)
+                .key(key)
+                .updateExpression("SET shipping_address = :newAddress")
+                .expressionAttributeValues(attributeValues)
+                .build();
+
+        dynamoDbClient.updateItem(request);
+    }
+}
